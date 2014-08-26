191011b8adbe3af0e245b6d2a20cf6e7c4b2bd3b
Phil <Phil.Czek@Gmail.com>
2014-08-26 23:13:58 +0200

    enhanced music
    added a couple new tracks and implemented random playback of these

---
+++ Assets/Scripts/RandomMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMusic : MonoBehaviour {
+
+	public AudioClip[] soundtrack;
+	// Use this for initialization
+	void Start () {
+		int trackNo = Random.Range(0, this.soundtrack.Length-1);
+		this.audio.clip = this.soundtrack[trackNo];
+		this.audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
