69db89a07508679e66474024f9668ceaa8044527
Phil <Phil.Czek@Gmail.com>
2014-08-27 12:15:57 +0200

    Music now plays continuously
    But just through the game and restart scene. In the start scene it is
    now playing, which is intended.
    Solved #9

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	private static MusicManager instance;
+	public AudioClip[] soundtrack;
+
+	void Awake(){
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start () {
+		if(!this.audio.isPlaying){
+			int trackNo = Random.Range(0, this.soundtrack.Length-1);
+			this.audio.clip = this.soundtrack[trackNo];
+			this.guiText.text = this.soundtrack[trackNo].name;
+			
+			this.audio.Play();
+		}	
+	}
+
+}
