df8be5ee310a3de0efddf4e7d098c43247406315
Phil <Phil.Czek@Gmail.com>
2014-08-29 22:15:04 +0200

    changed background to grass and electric windmills
    See ticket #4 for more information.
    But there are way more things to add to make it more vivid

--- Assets/Scripts/ParallaxScrolling.cs
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -3,15 +3,41 @@ using System.Collections;
 
 public class ParallaxScrolling : MonoBehaviour {
 
-	public int speed = 1;
+
+	public GameObject prefabObject;
+	public int prefabScale;
+	public float speed;
+	public bool random=false;
+	public int min=0;
+	public int max=50;
+
+
+
 	private GameObject player;
 	// Use this for initialization
 	void Start () {
+		// get player for player dependet parallax
 		this.player = GameObject.Find("/Tottens");
+
+		// generate a stream of ground texture
+		for(int i = 0; i < 100; i++){
+			SpriteRenderer sp = prefabObject.renderer as SpriteRenderer;
+			float screenWidth = sp.sprite.texture.width/this.prefabScale;
+			float xOffset;
+			if(this.random){
+				xOffset = Random.Range(this.min,this.max);
+			}else{
+				xOffset= sp.sprite.texture.width/this.prefabScale;
+			}
+			GameObject obj = Instantiate (prefabObject, prefabObject.transform.position + new Vector3(xOffset*i, 0, 0), prefabObject.transform.rotation) as GameObject;
+			 
+			obj.transform.parent = this.transform;
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position -= new Vector3(speed/10000f*this.player.rigidbody2D.velocity.x,0,0);
+		this.transform.position -= new Vector3(this.player.rigidbody2D.velocity.x*speed,0,0);
 	}
 }
