bc313fcb188f29d9ba84903bb6b2d3426ec7486c
Phil <Phil.Czek@Gmail.com>
2014-09-05 20:08:11 +0200

    Added Soundtrack to the Credit screen
    Now displaying the tracklist of the soundtrack in the credits
    also renamed the 'Music' folder into 'Soundtrack'

---
+++ Assets/Scripts/CreditText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioClip[] soundtrack = Resources.LoadAll<AudioClip>("Soundtrack");
+
+		string creditText ="Soundtrack:\n";
+		foreach (AudioClip track in soundtrack){
+			creditText +="\t"+ track.name + "\n";	
+		}
+
+		creditText += "\nCheck out their other stuff, it's awesome too!";
+		this.guiText.text = creditText;
+	}
+
+}
